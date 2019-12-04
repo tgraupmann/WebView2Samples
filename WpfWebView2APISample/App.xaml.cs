d1849e54f23df8e62b26aab2e7d28ae82ee9b85f
Timothy Graupmann <tim.graupmann@razer.com>
2019-12-04 13:11:37 -0800

    Merged sample from https://github.com/microsoft/WPF-Samples/tree/master/Migration%20and%20Interoperability/WPFHostingWin32Control

--- WpfWebView2APISample/App.xaml.cs
+++ WpfWebView2APISample/App.xaml.cs
@@ -1,9 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
+﻿// // Copyright (c) Microsoft. All rights reserved.
+// // Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.Runtime.InteropServices;
 using System.Windows;
 
 namespace WpfWebView2APISample
@@ -13,5 +11,11 @@ namespace WpfWebView2APISample
     /// </summary>
     public partial class App : Application
     {
+        [DllImport("comctl32.dll", EntryPoint = "InitCommonControls", CharSet = CharSet.Auto)]
+        public static extern void InitCommonControls();
+
+        private void ApplicationStartup(object sender, StartupEventArgs args)
+        {
+        }
     }
 }
